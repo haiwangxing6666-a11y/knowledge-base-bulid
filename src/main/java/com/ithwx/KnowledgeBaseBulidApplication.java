0d38f7c15f4c58900864fca6ec33b8a53f556ac5
haiwangxing6666-a11y <haiwangxing6666@163.com>
2026-08-31 16:29:05 +0800

    chore: 初始化 Spring Boot 项目

---
+++ src/main/java/com/ithwx/KnowledgeBaseBulidApplication.java
@@ -0,0 +1,13 @@
+package com.ithwx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeBaseBulidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeBaseBulidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ithwx/KnowledgeBaseBulidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ithwx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeBaseBulidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
